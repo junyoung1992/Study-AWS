d8fb9119343755598ae9704e28140b0cdc1267e5
Kim Junyoung <orsay0827@gmail.com>
2022-05-10 21:57:59 +0900

    first commit

---
+++ src/main/java/com/example/studyaws/StudyAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyaws/StudyAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
